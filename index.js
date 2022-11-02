75aa99be1e407cba4536eb68453bbc7bca136e9c
Abhay <abhay.pratap@apptile.io>
2022-11-02 22:53:42 +0530

    added android asset links

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
+app.use("*", (req, res, error) => {
+  res.status(200).json({ message: req.path });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
