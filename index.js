dd95e7652028f5ca2aab41b82c46210586ee1398
Abhay <abhay.pratap@apptile.io>
2022-11-24 16:26:33 +0530

    enabled cors

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+var cors = require("cors");
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(express.static("public"));
 
